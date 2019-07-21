695a365d8e1499c662b0996e71fa92ed9b35010e
thimi0412 <thimi0412@icloud.com>
2019-07-22 01:00:53 +0900

    update jwtで承認を行う

---
+++ authentication/jwt.go
@@ -0,0 +1,55 @@
+package authentication
+
+import (
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/thimi0412/gin-practice/db"
+	"github.com/thimi0412/gin-practice/entity"
+)
+
+// JWTToken jwt作成用
+type JWTToken struct {
+	UserID   int    `json:"user_id"`
+	Email    string `json:"e_mail"`
+	Password string `json:"password"`
+	jwt.StandardClaims
+}
+
+// CreateTokenString 承認用jwtを作成
+func CreateTokenString(user entity.User) (string, error) {
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), &JWTToken{
+		UserID:   user.ID,
+		Email:    user.Email,
+		Password: user.Password,
+	})
+	tokenString, err := token.SignedString([]byte("foobar"))
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+// AuthTokenString jwtが正しいかチェックする
+func AuthTokenString(tokenString string) error {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte("foobar"), nil
+	})
+	if err != nil {
+		return err
+	}
+	claims := token.Claims.(jwt.MapClaims)
+
+	claimsEmail := claims["email"].(string)
+	claimsPassword := claims["password"].(string)
+
+	conn := db.DBConnect()
+	defer conn.Close()
+
+	user := entity.User{}
+	user.Email = claimsEmail
+	user.Password = claimsPassword
+
+	if err := conn.First(&user).Error; err != nil {
+		return err
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/thimi0412/gin-practice
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.4.0
 	github.com/jinzhu/gorm v1.9.10
 	golang.org/x/crypto v0.0.0-20190325154230-a5d413f7728c
