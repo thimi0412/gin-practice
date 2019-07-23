89d45dc3af643774e7ab7fd6f00971b14f157b71
thimi0412 <thimi0412@icloud.com>
2019-07-23 14:39:48 +0900

    add todoのentityを追加

---
+++ entity/todo.go
@@ -0,0 +1,13 @@
+package entity
+
+import "time"
+
+// Todo todoプロパティ
+type Todo struct {
+	ID        int        `json:"id"`
+	UserID    string     `json:"user_id"`
+	Context   string     `json:"context"`
+	LimitDate *time.Time `json:"limit_date"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+}
